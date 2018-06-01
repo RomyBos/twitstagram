1841be1235445618ccbd5099eb98c73cb48aa9c6
Romy <romy.bos@hotmail.com>
2018-06-01 11:34:38 +0200

    [WIP] Now shows User, and puts tweets in a card. Still figuring out how to get whitespace between tweets(or get them in different cards)

--- Twitstagram/Twitstagram/Controllers/HomeController.cs
+++ Twitstagram/Twitstagram/Controllers/HomeController.cs
@@ -11,33 +11,33 @@ namespace Twitstagram.Controllers
     public class HomeController : Controller
     {
 
-    public ActionResult Index()
+
+        public ActionResult Index()
         {
 
             var twitter = new Twitter.Twitter("4rNfhgUrI6yklSBVhmU3U1F8q", "kYsjQzU7QVof9USCQHucFi3p2ox61q2GxNVYcDSzTiBzu0C0vT", "1001781311051390978-Tj0aXHjC3GSuqGFde9AFp6z9Wjp4i2", "BxYSpsScwSW9oxMIozpYpNv7efJhXIihvXduGgUU32S3K");
+            string user = "bencom_group";
 
-            //twitter.PostStatusUpdate(status, 54.35,-0.2);
-            var response = twitter.GetTweets("romybos_", 5);
+            var response = twitter.GetTweets(user, 5);
 
             List<string> TimeLine = new List<string>();
 
             dynamic timeline = System.Web.Helpers.Json.Decode(response);
 
 
+            
+
             foreach (dynamic tweet in timeline)
             {
                 string text = tweet.text;
-                string screen_name = tweet.user.screen_name;
-                TimeLine.Add(screen_name);
                 TimeLine.Add(text);
 
             }
 
-            string twitterfeed = "<p>" +string.Join("\n " + "<br />", TimeLine) + "</p>";
+            string twitterfeed = string.Join(System.Environment.NewLine, TimeLine);
 
-
+            ViewBag.Header = "@" + user;
             ViewBag.Message = (twitterfeed);
-            Console.WriteLine(twitterfeed);
 
             return View();
         } 
